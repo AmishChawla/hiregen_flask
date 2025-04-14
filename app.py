e9cb1c75765d89ecfcf4c83d9d6ec7684e12851d
AmishChawla <amishchawla7@gmail.com>
2025-04-14 16:08:21 +0530

    add post working hahaha

--- app.py
+++ app.py
@@ -33,10 +33,10 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 # csrf = CSRFProtect(app)
 #TODO CHANGE TO 'hiregen.com' before deploying
 # app.config['SERVER_NAME'] = 'localhost.com:5000'  # Base domain for subdomains
-# # app.config['SERVER_NAME'] = 'hiregen.com'  
+app.config['SERVER_NAME'] = 'hiregen.com'  
 #TODO CHANGE TO '.hiregen.com' before deploying
 # app.config['SESSION_COOKIE_DOMAIN'] = '.localhost.com'  # Leading dot to share session across subdomains
-# # app.config['SESSION_COOKIE_DOMAIN'] = '.hiregen.com'  # Leading dot to share session across subdomains  
+app.config['SESSION_COOKIE_DOMAIN'] = '.hiregen.com'  # Leading dot to share session across subdomains  
 
 app.config['SESSION_COOKIE_PATH'] = '/'
 #TODO UNCOMMENT BEFORE DEPLOYING
